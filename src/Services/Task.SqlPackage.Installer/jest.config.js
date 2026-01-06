9724431d28f5ff3ac1c1695b4c8c38175237408d
Pedro Sánchez <pedrocesar88@gmail.com>
2026-01-06 09:52:25 -0400

    Bump version to 1.0.28 and update coverage configuration

--- src/Services/Task.SqlPackage.Installer/jest.config.js
+++ src/Services/Task.SqlPackage.Installer/jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   roots: ['<rootDir>/__tests__'],
   testMatch: ['**/__tests__/**/*.test.ts'],
   collectCoverageFrom: [
-    'index.ts',
+    '<rootDir>/index.ts',
     '!**/*.d.ts',
     '!**/node_modules/**',
   ],
